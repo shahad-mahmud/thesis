d82ae87616a08d6585667271cc59a8d291e0ed8a
shahad-mahmud <shahad9381@gmail.com>
2020-04-10 12:43:09 +0600

    tweet scraper

---
+++ COVID_19/tweepy_test.py
@@ -0,0 +1,111 @@
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import tweepy
+import json
+import pandas as pd
+import csv
+import re
+from textblob import TextBlob
+import string
+import preprocessor as p
+import os
+import time
+from datetime import datetime
+
+# Twitter credentials for the app
+consumer_key = 'GWjJzHWaHOmNycuNtlyKI9F0U'
+consumer_secret = 'SNPAyWBfjOXDAZXELy3m8CWXjzDl1NP22SIPZEbudz9A2LOrMO'
+access_key = '368305011-U0G3Cs9hdu5GymkknoroHrVI5Tbywh6aSzGCqDhT'
+access_secret = 'iELOscP633TfOh2DyWhlfQu4FrZoaZuMVWynpUGgSjrmg'
+
+# pass twitter credentials to tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+
+def scrap_tweets(search_words, date_since, numTweets, numRuns):
+    db_tweets = pd.DataFrame(columns=['tweet_id', 'user_name', 'user_location', 'tweet_time', 're_tweets',
+                                      'tweet_text', 'hash_tags'])
+    # Define a for-loop to generate tweets at regular intervals
+    for i in range(0, numRuns):
+        # We will time how long it takes to scrape tweets for each run:
+        start_run = time.time()
+        tweets = tweepy.Cursor(api.search, q=search_words, lang="en", since=date_since, tweet_mode='extended').items(
+            numTweets)
+        tweet_list = [tweet for tweet in tweets]
+
+        # Obtain the following info (methods to call them out):
+        # user.screen_name - twitter handle
+        # user.description - description of account
+        # user.location - where is he tweeting from
+        # user.friends_count - no. of other users that user is following (following)
+        # user.followers_count - no. of other users who are following this user (followers)
+        # user.statuses_count - total tweets by user
+        # user.created_at - when the user account was created
+        # created_at - when the tweet was created
+        # retweet_count - no. of retweets
+        # (deprecated) user.favourites_count - probably total no. of tweets that is favourited by user
+        # retweeted_status.full_text - full text of the tweet
+        # tweet.entities['hashtags'] - hashtags in the tweet
+
+        # Begin scraping the tweets individually:
+        noTweets = 0
+
+        for tweet in tweet_list:
+            # print(tweet)
+
+            # Pull the values
+            tweet_id = tweet.id
+            username = tweet.user.screen_name
+            location = tweet.user.location
+            tweetcreatedts = tweet.created_at
+            retweetcount = tweet.retweet_count
+            hashtags = tweet.entities['hashtags']
+
+            try:
+                text = tweet.retweeted_status.full_text
+            except AttributeError:  # Not a Retweet
+                text = tweet.full_text
+
+            # Add the 11 variables to the empty list - ith_tweet:
+            ith_tweet = [tweet_id, username, location, tweetcreatedts, retweetcount, text, hashtags]
+
+            # Append to dataframe - db_tweets
+            db_tweets.loc[len(db_tweets)] = ith_tweet
+
+            # increase counter - noTweets
+            noTweets += 1
+
+        # Run ended:
+        end_run = time.time()
+        duration_run = round(end_run - start_run, 2)
+
+        print('no. of tweets scraped for run {} is {}'.format(i, noTweets))
+        print('time take for {} run to complete is {}'.format(i, duration_run))
+
+        time.sleep(920)  # 15 minute sleep time
+
+    # Once all runs have completed, save them to a single csv file:
+    # Obtain timestamp in a readable format:
+    from datetime import datetime
+    to_csv_timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
+
+    # Define working path and filename
+    path = os.getcwd()
+    filename = path + '/data/' + to_csv_timestamp + '_covid_19_tweets.csv'
+
+    # Store dataframe in csv with creation date timestamp
+    db_tweets.to_csv(filename, index=False)
+
+    print('Scraping has completed!')
+
+
+# Initialise these variables:
+search_words = "#corona OR #covid OR #covid19 OR #COVIDー19 OR #covid-19 OR #CoronaVirus OR" \
+               "#StayHome OR #StayAtHome OR #StayHomeStaySafe"
+date_since = "2019-11-03"
+numTweets = 2500
+numRuns = 6
+# Call the function scraptweets
+scrap_tweets(search_words, date_since, numTweets, numRuns)
--- COVID_19/tweet_scraper.py
+++ COVID_19/tweet_scraper.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import os
 
-no_of_tweets_to_extract = 50000
+no_of_tweets_to_extract = 3000
+rows_to_skip = 11000
 
-id_file = pd.read_csv("../../data sets/corona_tweets_01.csv", usecols=[0], nrows=no_of_tweets_to_extract)
-
+id_file = pd.read_csv("../../data sets/corona_tweets_01.csv", usecols=[0],
+                      nrows=rows_to_skip + no_of_tweets_to_extract +5)
+print(id_file.head(5))
 tweet_ids = []
 tweets = []
 no_of_extracted_tweets = 0
+# file = open('tweets_01.csv', 'a')
 
-for tweet_id in id_file['tweet_id']:
+# os.remove("extracted_tweets_01.csv")
+
+for tweet_id in id_file['tweet_id'][rows_to_skip:rows_to_skip + no_of_tweets_to_extract]:
+    #     file = open('tweets_01.csv', 'a')
     print('Extracted ', no_of_extracted_tweets, ' of ', no_of_tweets_to_extract, ' tweets.\n',
           (no_of_extracted_tweets / no_of_tweets_to_extract) * 100, '% done')
     tweet_url = 'https://twitter.com/user/status/' + str(tweet_id)
@@ -19,13 +26,25 @@ for tweet_id in id_file['tweet_id']:
     tweet = page_content.find_all('p', {'class': 'TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text'})
     # print(tweet_id, ' -> ', tweet)
     if len(tweet) > 0:
+        #         s = str(tweet_id) + ',' + tweet[0].text +'\n'
+        #         file.write(s)
         tweet_ids.append(tweet_id)
         tweets.append(tweet[0].text)
+    if no_of_extracted_tweets % 5 == 0:
+        print('Adding to data set...')
+        d = {'tweet_id': tweet_ids, 'tweet': tweets}
+        df = pd.DataFrame(d)
+        df.to_csv('../../data sets/working/extracted_tweets_01_short.csv', mode='a', header=False, index=False)
+        tweet_ids = []
+        tweets = []
+        print('Added to data set!\n')
     no_of_extracted_tweets = no_of_extracted_tweets + 1
     # print(tweet_id, " -> ", tweet[0].text)
+#     file.close()
 
-print('Tweets extraction completed. Creating data set...')
+print('Tweets extraction completed. Completiong data set...')
 tweet_dic = {'tweet_id': tweet_ids, 'tweet': tweets}
 df = pd.DataFrame(tweet_dic)
-df.to_csv('../../data sets/test.csv')
-print('Data set created')
+df.to_csv('../../data sets/working/extracted_tweets_01_short.csv', mode='a', header=False, index=False)
+# file.close()
+print('Data set completed')
